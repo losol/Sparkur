de86a4218d1533133e632c298b33d2346e56c13b
Ole Kristian Losvik <losvik@gmail.com>
2019-06-30 17:00:49 +0200

    Renamed API

--- Startup.cs
+++ Startup.cs
@@ -135,7 +135,7 @@ namespace Sparkur
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "FHIR API");
             });
 
             app.UseMvc();
