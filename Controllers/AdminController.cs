f47fbd850a03f9f05720f682fdfa7d38261b4472
Ole Kristian Losvik <losvik@gmail.com>
2019-07-01 10:27:40 +0200

    Added verbs

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -9,12 +9,13 @@ namespace Sparkur.Areas.Admin.Controllers
 {
 	[Authorize(Policy = "RequireAdministratorRole")]
 	public class AdminController : Controller {
-
+			[HttpGet]
 			public IActionResult Index()
 			{
 				return View();
 			}
 			
+			[HttpGet]
 			public IActionResult Maintenance()
 			{
 				return View();
